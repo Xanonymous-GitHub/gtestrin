71b51a896b7bf48959312f424078c5c567c9d1f1
Xanonymous <trusaidlin@gmail.com>
2022-12-26 03:59:58 +0800

    chore: format files

--- distribute.sh
+++ distribute.sh
@@ -6,12 +6,21 @@ function gen_hash() {
 
 IMAGE_NAME=xanonymous/gtest-cpp-essential-env
 
+set -e
+
+# remove the temporary builder
+docker buildx rm tmp_getstrin_builder -f
+
 # create a temporary builder
 docker buildx create --name tmp_getstrin_builder --use
 
+# pull all tags from remote.
+docker pull --all-tags $IMAGE_NAME
+
 # build the image.
 docker buildx build \
-  --platform linux/amd64,linux/arm64,linux/arm/v7 \
+  --platform \
+  linux/amd64,linux/arm64,linux/arm/v7 \
   -t ${IMAGE_NAME}:latest \
   -t ${IMAGE_NAME}:"$(gen_hash)" \
   --push .
